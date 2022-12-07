7f73dd10e722cef357b8f9d1b6a96d3df21d52e5
NicolasRodriguezSteuerberg <nrodriguezsteuerberg@danielcastelao.org>
2022-12-07 14:14:06 +0100

    Inicio

---
+++ CreandoConflictos/src/Main.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int num;
+
+        num=Integer.parseInt(JOptionPane.showInputDialog("Teclee 1 o 2"));
+
+        if (num==1){
+            JOptionPane.showMessageDialog(null,"Eres una persona genial");
+        }
+
+        else if(num==2){
+            JOptionPane.showMessageDialog(null,"Eres buena gente");
+        }
+
+        else{
+            JOptionPane.showMessageDialog(null,"Eres mala persona por no hacer lo que he dicho");
+        }
+    }
+}
